9bf0f707a631ad00625034bf8cbafedcddac9b4a
Terje Uglebakken <teug91@gmail.com>
2019-09-16 23:15:21 +0530

    use env vars for server url

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import { CssReset } from '@dhis2/ui-core'
 import { Main } from './Main'
 import 'typeface-roboto'
 
-// The two props can be removed if they are not needed.
-export const App = ({ baseUrl, apiVersion }) => (
+export const App = () => (
     <>
         <CssReset />
         <Main />
--- src/index.js
+++ src/index.js
@@ -2,35 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/App'
 
-/**
- * Passed to DataProvider to get various data for the header bar.
- * If there is another minor release to for example 2.31.6,
- * then this variable must be updated.
- */
-const developmentServer = 'https://play.dhis2.org/2.31.5'
-
-/**
- * Passed to DataProvider to get various data for the header bar.
- */
-const apiVersion = 31
-
 const rootElement = document.getElementById('root')
 
-const productionRender = async () => {
-    try {
-        const manifest = await (await fetch('./manifest.webapp')).json()
-        render(manifest.activities.dhis.href)
-    } catch (error) {
-        console.error('Could not read manifest:', error)
-        ReactDOM.render(<code>No manifest found</code>, rootElement)
-    }
-}
-
-const render = baseUrl =>
-    ReactDOM.render(
-        <App baseUrl={baseUrl} apiVersion={apiVersion} />,
-        rootElement
-    )
-
-if (process.env.NODE_ENV === 'production') productionRender()
-else render(developmentServer)
+ReactDOM.render(<App />, rootElement)
