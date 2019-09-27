c4a9683010a761c755495eba917f88bac954aa45
Terje Uglebakken <teug91@gmail.com>
2019-09-27 13:30:31 +0200

    feat: add query

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Provider } from '@dhis2/app-runtime'
 import { CssReset } from '@dhis2/ui-core'
 import { Main } from './Main'
 import 'typeface-roboto'
 
+const config = {
+    baseUrl: process.env.REACT_APP_DHIS2_BASE_URL,
+    apiVersion: process.env.REACT_APP_DHIS2_API_VERSION,
+}
+
 export const App = () => (
-    <>
+    <Provider config={config}>
         <CssReset />
         <Main />
-    </>
+    </Provider>
 )
